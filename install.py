328bc13f79dcf94f4c13a19c5ff15f675651f0a4
Black-Cog <blackcogdigital@gmail.com>
2015-03-20 07:52:12 +0100

    Update install to be extern

--- install.py
+++ install.py
@@ -1,14 +1,57 @@
-import subprocess, shlex, os
+import subprocess
+import shlex
+import os
+import sys
+
+parentPath = '/'.join( sys.path[0].replace('\\', '/').split('/')[:-1] )
+sys.path.append( parentPath )
+
+import Forge
+
+
+###############################################################################################
+# Version
+###############################################################################################
+
+
+bcShadingMilestoneVersion = 0 # for announcing major milestones - may contain all of the below
+bcShadingMajorVersion     = 0 # backwards-incompatible changes
+bcShadingMinorVersion     = 0 # new backwards-compatible features
+bcShadingPatchVersion     = 1 # bug fixes
+
+
+###############################################################################################
+# Environment
+###############################################################################################
+
+
+softwareEnvironment = 'f:/software/'
+softwareName = 'bcShading_%s.%s.%s.%sdev' %( bcShadingMilestoneVersion, bcShadingMajorVersion, bcShadingMinorVersion, bcShadingPatchVersion )
+softwarePath = '%s%s/' %( softwareEnvironment, softwareName )
+
+
+###############################################################################################
+# Folder creation
+###############################################################################################
+
 
 rslDir = 'rsl'
 sdlDir = 'sdl'
 shaderCount = 0
 
+Forge.core.System().mkdir( '%s%s' %(softwarePath, sdlDir) )
+
+
+###############################################################################################
+# Shader compilation
+###############################################################################################
+
+
 print '>>> Compile Begin'
 
 for file in os.listdir( rslDir ):
     if '.sl' in file:
-		cmd = '"C:/Program Files/3Delight For Maya/bin/shaderdl-3dfm.exe" -d %s %s/%s' % ( sdlDir, rslDir, file )
+		cmd = '"C:/Program Files/3Delight For Maya/bin/shaderdl-3dfm.exe" -d %s%s %s/%s' % ( softwarePath, sdlDir, rslDir, file )
 		args = shlex.split(str(cmd))
 		subprocess.Popen(args)
 		shaderCount += 1
